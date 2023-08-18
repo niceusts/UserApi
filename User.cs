d9d1f6da67ba698cdc9bc6fe02cb7b2fc02d66ea
Niceu Santos Biriba <51482169+niceusts@users.noreply.github.com>
2023-08-17 22:16:15 -0300

    Api com Post e Get funcionando

--- User.cs
+++ User.cs
@@ -5,7 +5,7 @@ namespace UserApi.Models
 	public class User
 	{
 		public int Cpf { get; set; }
-		public string? Nome { get; set; }
+		public string? Nome { get; set; }  //user type
 		public DateTime DataNascimento { get; set; }
 	}
 }
